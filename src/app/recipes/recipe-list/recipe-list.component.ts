70d6f46f9fd038881951179d2dd3d6d6d73875c8
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2018-02-10 10:29:16 +0000

    Added DropdownService.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,8 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Mango Curry', 'lovely curry',
     'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-0UEdp10cF1s%2FTdOc8GOMVaI%2FAAAAAAAAL3s%2FWZpSadpzpwc%2Fs1600%2FRipe%2BMango%2BCurry%2B-1.jpg&f=1' ),
-    new Recipe('Lentil Ragu', 'tasty pasta', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--563473_12.jpg?itok=EOUyLmkO'),
+    new Recipe('Lentil Ragu', 'tasty pasta', 
+    'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--563473_12.jpg?itok=EOUyLmkO'),
   ];
 
   onRecipeSelected(recipe: Recipe) {
